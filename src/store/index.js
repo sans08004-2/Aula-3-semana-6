ea415411fef8e42d82b88a46f974bfd3b139ccef
sans08004-2 <00001123911654sp@al.educacao.sp.gov.br>
2026-03-26 14:02:18 -0300

    Create index.js
    criei e coloquei o codigo

---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    cart: []
+  },
+  mutations: {
+    ADD_TO_CART(state, product) {
+      state.cart.push(product)
+    }
+  },
+  actions: {
+    addToCart({ commit }, product) {
+      commit('ADD_TO_CART', product)
+    }
+  },
+  getters: {
+    cartItemCount: state => state.cart.length
+  }
+})
