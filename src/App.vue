89347a2279bbdce4688568cebaf0664b79307698
sans08004-2 <00001123911654sp@al.educacao.sp.gov.br>
2026-03-26 14:07:13 -0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Meu App</h1>
+    <ShoppingCart />
+  </div>
+</template>
+
+<script>
+import ShoppingCart from './components/ShoppingCart.vue'
+
+export default {
+  components: {
+    ShoppingCart
+  }
+}
+</script>
