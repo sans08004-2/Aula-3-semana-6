98aa4458ee78b5952b22c5018c1d4b79a83d7aa0
sans08004-2 <00001123911654sp@al.educacao.sp.gov.br>
2026-03-26 14:00:42 -0300

    Create ShoppingCart.vue
    criei e coloquei o codigo

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="add">Adicionar produto</button>
+    <p>Total: {{ total }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    total() {
+      return this.$store.getters.cartItemCount
+    }
+  },
+  methods: {
+    add() {
+      this.$store.dispatch('addToCart', { nome: 'Produto' })
+    }
+  }
+}
+</script>
